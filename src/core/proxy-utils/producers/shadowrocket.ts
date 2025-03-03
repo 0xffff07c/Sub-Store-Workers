1c5d03a46480578709311037df8600afd96d6d37
0xffff07c <licoln.lee.us@outlook.com>
2025-03-04 02:03:56 +0800

    Update shadowrocket.ts

--- src/core/proxy-utils/producers/shadowrocket.ts
+++ src/core/proxy-utils/producers/shadowrocket.ts
@@ -1,4 +1,4 @@
-import { isPresent } from '@/core/proxy-utils/producers/utils';
+import { isPresent } from '../../../core/proxy-utils/producers/utils';
 
 export default function Shadowrocket_Producer() {
     const type = 'ALL';
