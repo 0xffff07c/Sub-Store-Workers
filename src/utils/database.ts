ed81bc431888b307e4946c51d5e95cba705362eb
0xffff07c <licoln.lee.us@outlook.com>
2025-03-04 00:24:39 +0800

    Update database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,19 +1,17 @@
-export function findByName(list: Array<any>, name: any) {
-    return [...list].find((item) => item.name === name);
+export function findByName(list, name, field = 'name') {
+    return list.find((item) => item[field] === name);
 }
 
-export function findIndexByName(list: Array<any>, name: any) {
-    return [...list].findIndex((item) => item.name === name);
+export function findIndexByName(list, name, field = 'name') {
+    return list.findIndex((item) => item[field] === name);
 }
 
-export function deleteByName(list: Array<any>, name: any) {
-    list = [...list];
-    const idx = findIndexByName(list, name);
+export function deleteByName(list, name, field = 'name') {
+    const idx = findIndexByName(list, name, field);
     list.splice(idx, 1);
-    return list;
 }
 
-export function updateByName(list: Array<any>, name: any, newItem: any) {
-    const idx = findIndexByName(list, name);
-    return [...list][idx] = newItem;
+export function updateByName(list, name, newItem, field = 'name') {
+    const idx = findIndexByName(list, name, field);
+    list[idx] = newItem;
 }
