8ddf907acc302019491aef39abfdbf09e160a3c7
0xffff07c <licoln.lee.us@outlook.com>
2025-03-04 01:01:56 +0800

    Update and rename node-info.ts to node-info.js

--- src/restful/node-info.js
+++ src/restful/node-info.js
@@ -1,16 +1,15 @@
-import producer from '../core/proxy-utils/producers';
-import { HTTP } from '../vendor/open-api';
-import { failed, success } from './response';
-import { NetworkError } from './errors';
-import { Context, Hono } from 'hono';
+import producer from '@/core/proxy-utils/producers';
+import { HTTP } from '@/vendor/open-api';
+import { failed, success } from '@/restful/response';
+import { NetworkError } from '@/restful/errors';
 
-export default async function register($app: Hono) {
+export default function register($app) {
     $app.post('/api/utils/node-info', getNodeInfo);
 }
 
-const getNodeInfo = async (c: Context) => {
-    const proxy = await c.req.json();
-    const lang = c.req.query('lang') || 'zh-CN';
+async function getNodeInfo(req, res) {
+    const proxy = req.body;
+    const lang = req.query.lang || 'zh-CN';
     let shareUrl;
     try {
         shareUrl = producer.URI.produce(proxy);
@@ -23,31 +22,33 @@ const getNodeInfo = async (c: Context) => {
         const info = await $http
             .get({
                 url: `http://ip-api.com/json/${encodeURIComponent(
-                    proxy.server,
+                    `${proxy.server}`
+                        .trim()
+                        .replace(/^\[/, '')
+                        .replace(/\]$/, ''),
                 )}?lang=${lang}`,
                 headers: {
                     'User-Agent':
                         'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
                 },
             })
-            .then(
-                (resp: Response) => resp.json()
-            )
-            .then((data) => {
+            .then((resp) => {
+                const data = JSON.parse(resp.body);
                 if (data.status !== 'success') {
                     throw new Error(data.message);
                 }
+
                 // remove unnecessary fields
                 delete data.status;
                 return data;
             });
-        return success(c, {
+        success(res, {
             shareUrl,
             info,
         });
     } catch (err) {
-        return failed(
-            c,
+        failed(
+            res,
             new NetworkError(
                 'FAILED_TO_GET_NODE_INFO',
                 `Failed to get node info`,
